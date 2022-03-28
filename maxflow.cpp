5ebbd07748286fcd88731e88b03809520ec4b421
Hiroshi Wu <buhiroshi@hotmail.com>
2022-03-28 01:01:12 -0400

    Added min cost max flow to max flow template

--- maxflow.cpp
+++ maxflow.cpp
@@ -9,25 +9,20 @@ public:
 
   struct edge {
     int u, v;
-    ll cap, flow;
+    ll cap, flow, cost;
     struct edge *rev = NULL;
-    edge(int a, int b, ll c, ll d) {
-      u = a; v = b; cap = c, flow = d;
+    edge(int from, int to, ll capacity, ll c) {
+      u = from; v = to; cap = capacity; flow = 0; cost = c;
     }
   };
 
-  MaxFlow(vector<int>& layersizes) {
-    layerindices.resize(layersizes.size());
-    int psum = 2;
-    for (int i = 0; i < layersizes.size(); i++) {
-      layerindices[i] = psum;
-      psum += layersizes[i];
-    }
-    numnodes = psum;
+  int n;
+  bool hasnegativecost = false;
+  vector<vector<edge*>> g;
 
-    g.resize(numnodes);
-    dist.resize(numnodes);
-    current.resize(numnodes);
+  MaxFlow(int numnodes) {
+    n = numnodes;
+    g.resize(n, vector<edge*>());
   }
   ~MaxFlow() {
     for (int i = 0; i < g.size(); i++) {
@@ -37,70 +32,62 @@ public:
     }
   }
 
-  void add_edge(int l1, int i1, int l2, int i2, ll cap) {
-    int u = getidx(l1, i1);
-    int v = getidx(l2, i2);
-    struct edge *forward = new struct edge(u,v,cap,0);
-    struct edge *backward = new struct edge(v,u,0,0);
+  void add_edge(int u, int v, ll cap) {
+    add_edge_internal(u,v,cap,1);
+  }
+  void add_edge(int u, int v, ll cap, ll cost) {
+    if (cost < 0) hasnegativecost = true;
+    add_edge_internal(u,v,cap,cost);
+  }
+  void add_edge_internal(int u, int v, ll cap, ll cost) {
+    struct edge *forward = new struct edge(u,v,cap,cost);
+    struct edge *backward = new struct edge(v,u,0,-cost);
     forward->rev = backward;
     backward->rev = forward;
     g[u].push_back(forward);
     g[v].push_back(backward);
   }
 
-  ll dinic() {
+  ll dinic(int source, int sink) {
     ll flow = 0;
     while (true) {
       // 1. build layer graph
-      fill(dist.begin(), dist.end(), -1);
-      dist[0] = 0;
-      bfs();
-      if (dist[1] == -1) return flow;
-
+      vector<int> dist(n, -1);
+      dist[source] = 0;
+      bfs(source, dist);
+      if (dist[sink] == -1) return flow;
       // 2. find blocking flow
-      fill(current.begin(), current.end(), 0);
-      ll f = blockingflow();
+      ll f = blockingflow(source, sink, dist);
       flow += f;
     }
     return flow;
   }
 
-  int numnodes;
-  vector<int> layerindices;
-  vector<vector<edge*>> g;
-  vector<int> dist;
-  vector<int> current;
-
-  int getidx(int l, int i) {
-    if (l == -1) return i;
-    else return layerindices[l] + i;
-  }
-
-  void bfs() {
+  void bfs(int source, vector<int>& dist) {
     deque<int> q;
-    q.push_back(0);
+    q.push_back(source);
     while (!q.empty()) {
       int u = q.front();
       q.pop_front();
-      for (int i = 0; i < g[u].size(); i++) {
-        struct edge *e = g[u][i];
+      for (edge *e : g[u]) {
         if (e->cap - e->flow > 0 && dist[e->v] == -1) {
-          dist[e->v] = dist[u] + 1;
+          dist[e->v] = dist[e->u] + 1;
           q.push_back(e->v);
         }
       }
     }
   }
 
-  ll blockingflow() {
+  ll blockingflow(int source, int sink, vector<int>& dist) {
+    vector<int> current(n, 0);
     ll total = 0;
     while (true) {
-      ll f = dfs(0);
+      ll f = dfs(source, sink, dist, current);
       if (f == 0) return total;
       total += f;
-      int u = 0;
-      while (u != 1) {
-        struct edge *e = g[u][current[u]];
+      int u = source;
+      while (u != sink) {
+        edge *e = g[u][current[u]];
         e->flow += f;
         e->rev->flow -= f;
         u = e->v;
@@ -108,17 +95,96 @@ public:
     }
   }
 
-  ll dfs(int u) {
-    if (u == 1) return LLONG_MAX;
+  ll dfs(int u, int sink, vector<int>& dist, vector<int>& current) {
+    if (u == sink) return LLONG_MAX;
     while (current[u] < g[u].size()) {
-      struct edge *e = g[u][current[u]];
+      edge *e = g[u][current[u]];
       if (dist[e->v] > dist[e->u] && e->cap - e->flow > 0) {
-        ll f = dfs(e->v);
+        ll f = dfs(e->v, sink, dist, current);
         if (f > 0) return min(f, e->cap-e->flow);
       }
       current[u]++;
     }
     return 0;
+  }
+
+  pair<ll,ll> mcmf(int source, int sink) {
+    vector<ll> phi(n, 0);
+    if (hasnegativecost) {
+      bellman_ford(source, phi);
+    }
+
+    ll totalflow = 0;
+    ll totalcost = 0;
+    vector<ll> dist(n);
+    vector<edge*> parentedge(n);
+    while (true) {
+      fill(dist.begin(), dist.end(), LLONG_MAX);
+      dijkstra(source, sink, dist, phi, parentedge);
+      if (dist[sink] == LLONG_MAX) break;
+      // update potentials
+      for (int i = 0; i < n; i++) {
+        if (dist[i] != LLONG_MAX) phi[i] += dist[i];
+      }
+      // determine amount of flow to push
+      ll minf = LLONG_MAX;
+      for (int u = sink; u != source; u = parentedge[u]->u) {
+        minf = min(minf, parentedge[u]->cap - parentedge[u]->flow);
+      }
+      // push flow
+      for (int u = sink; u != source; u = parentedge[u]->u) {
+        parentedge[u]->flow += minf;
+        parentedge[u]->rev->flow -= minf;
+        totalcost += parentedge[u]->cost * minf;
+      }
+      totalflow += minf;
+    }
+
+    return make_pair(totalflow, totalcost);
+  }
+
+  void bellman_ford(int source, vector<ll>& phi) {
+    vector<vector<ll>> dp(n, vector<ll>(n, LLONG_MAX));
+    fill(dp[source].begin(), dp[source].end(), 0);
+    for (int k = 1; k < n; k++) {
+      for (int u = 0; u < n; u++) {
+        dp[u][k] = dp[u][k-1];
+      }
+      for (int u = 0; u < n; u++) {
+        for (edge *e : g[u]) {
+          if (e->flow < e->cap) dp[e->v][k] = min(dp[e->v][k], dp[u][k-1] + e->cost);
+        }
+      }
+    }
+    for (int i = 0; i < n; i++) {
+      phi[i] = dp[i][n-1];
+    }
+  }
+
+  void dijkstra(int source, int sink, vector<ll>& dist, vector<ll>& phi, vector<edge*>& parentedge) {
+    vector<bool> visited(n, false);
+    dist[source] = 0;
+    priority_queue<pair<ll,int>> q;
+    q.push(make_pair(0,source));
+    while (!q.empty()) {
+      pair<ll,int> p = q.top();
+      q.pop();
+      ll currdist = -p.first;
+      int u = p.second;
+      if (visited[u]) continue;
+      visited[u] = true;
+      if (u == sink) return;
+      for (edge *e : g[u]) {
+        if (e->flow >= e->cap) continue;
+        int v = e->v;
+        ll newdist = dist[u] + e->cost + phi[u] - phi[v];
+        if (newdist < dist[v]) {
+          dist[v] = newdist;
+          parentedge[v] = e;
+          q.push(make_pair(-newdist, v));
+        }
+      }
+    }
   }
 
 };
